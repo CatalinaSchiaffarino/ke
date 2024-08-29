01d84c6e858ac507587739d088baf2a8da92519e
catalinaschiaffarino <catalinaschiaffarinomartinez@gmail.com>
2024-08-29 20:29:46 -0300

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    let loginBtn = document.getElementById("loginBtn");
+    loginBtn.addEventListener("click", () => {
+        let usuario = document.getElementById("usuario").value;
+        let contraseña = document.getElementById("contraseña").value;
+          //Guardo Sesion con localStorage
+          localStorage.setItem("usuario",JSON.stringify(usuario));
+          localStorage.setItem("contraseña",JSON.stringify(contraseña));
+          let ObjUsuario = JSON.parse(localStorage.getItem("usuario"));
+          let ObjContraseña = JSON.parse(localStorage.getItem("contraseña"));
+          if (usuario!="" && contraseña!=""){
+            window.location.href = "index.html";
+        };
+
+    });
+});
